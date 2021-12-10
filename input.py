03537ee25aea7d9dd7a8c5598388fa359dba4bd3
fresh <79736815+fresh@users.noreply.github.com>
2021-12-09 22:19:20 -0500

    How to take user input

---
+++ input.py
@@ -0,0 +1,13 @@
+"""Taking user input"""
+
+str_input = str(input("This will be a string: "))
+#Takes string input and prints it
+print(str_input)
+
+int_input = int(input("This will be a integer: "))
+#Takes integer input and prints it
+print(int_input)
+
+float_input = float(input("This will be a float: "))
+#Takes float input and prints it
+print(float_input)
